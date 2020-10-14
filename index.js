eb4d96b07dc6d8df3f7255c7da1be99d3022fb2d
Faris Ismail <faris.ismail@cadcom.cz>
2020-10-14 21:03:55 +0200

    BodyParser

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.get('/ping', (request, response) => {
- response.send('pong');
+
+express.urlencoded(); //urlencoded parses x-ww-form-urlencoded request bodies
+app.use(express.json()); //json parses application/json request bodies
+
+
+app.get("/ping", (request, response) => {
+  response.send("pong");
 });
-app.listen(8080, 'localhost');
+
+app.post("/course", (req, res) => {
+  console.log(req.body);
+  res.json({ status: true });
+});
+
+app.listen(8080, "localhost");
