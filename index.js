e74c4dc54677190bfbcf0e0a7b3c5d3c68883d88
Faris Ismail <faris.ismail@cadcom.cz>
2020-10-14 21:15:57 +0200

    NODE_ENV Babel

--- index.js
+++ index.js
@@ -1,74 +1,30 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const cowsay = require("cowsay");
+
+console.log(process.env.NODE_ENV);
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello	World\n");
+});
+server.listen(port, hostname, () => {
+  console.log(`Server	running	at	http://${hostname}:${port}/`);
+  console.log(
+    cowsay.say({
+      text: "I'm a moooodule",
+      e: "oO",
+      T: "U ",
+    })
+  );
+});
+
 const express = require("express");
 const app = express();
-const jwt = require("jsonwebtoken");
-const axios = require("axios");
-
-const bodyParser = require("body-parser");
-const myPrivateSecret = "secret";
-
-app.use(bodyParser.json()); //for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencoded
-app.use("/private", (req, res, next) => {
-  if (req.body.token) {
-    const token = req.body.token;
-    try {
-      const decoded = jwt.verify(token, myPrivateSecret);
-      req.body.decoded = decoded;
-      next();
-    } catch (e) {
-      console.log(e);
-      res.sendStatus(403);
-    }
-  } else {
-    res.sendStatus(403);
-  }
+app.get("/", (req, res) => {
+  console.log(req.query);
+  res.send("hello");
 });
-
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-app.post("/public/login", (req, res) => {
-  const { user, password } = req.body;
-  //const user = await db.get(...);
-  if (user === "nodejs" && password === "mojeheslo") {
-    const token = jwt.sign({ userId: 5 }, myPrivateSecret, { expiresIn: 3600 });
-    res.json({ token });
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-app.post("/private/reporting", async (req, res) => {
-  try {
-    const { decoded } = req.body;
-    const userProfile = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${decoded.userId}`
-    );
-    console.log("HERE");
-    res.json({ userProfile: userProfile.data });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-app.post("/save", (req, res) => {
-  const { course } = req.body;
-  if (course === "NodeJS") {
-    res.json({
-      answer: "Hurááá",
-    });
-  } else {
-    res.json({
-      answer: "Máš být asi o třídu vedle",
-    });
-  }
-});
-
-app.put("/myput", (req, res) => {
-  res.sendStatus(404);
-});
-
-const port = 8080;
-app.listen(port, "localhost");
-console.log(`server listening on port: ${port}`);
+app.listen(8080);
+console.log("app listening");
