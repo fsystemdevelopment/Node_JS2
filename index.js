23fab50a9d1bb47d13f52b2c40c223de584496ce
faris <faris.ismail@cadcom.cz>
2022-09-13 07:57:09 +0200

    mjs node 18

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require("express");
+// const myLib = require("./config");
+// console.log(myLib);
+
+import { PI } from "./esmodule.mjs";
+import fs from "fs";
+
+import express from "express";
+//const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/ping", (request, response) => {
@@ -11,7 +18,10 @@ app.get("/ping", (request, response) => {
 app.post("/course", (req, res) => {
   console.log(req?.body);
   console.log(req?.body?.aaa);
+  fs.writeFileSync("./summary.txt", JSON.stringify(req.body));
   res.json({ status: true });
 });
 
-app.listen(8080, "localhost");
+app.listen(8080, "localhost", () => {
+  console.log("running");
+});
