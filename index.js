bed38e0b62259bfcada938e15a9f525c84deec61
faris <faris.ismail@cadcom.cz>
2022-11-25 05:53:35 +0100

    Multer

--- index.js
+++ index.js
@@ -2,73 +2,33 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 const axios = require("axios");
+const cors = require("cors");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    const extLength = file.originalname?.split(".")?.length;
+    const extension = file.originalname?.split(".")[extLength - 1];
+    cb(null, file.fieldname + "-" + Date.now() + "." + extension);
+  },
+});
 
+const upload = multer({ storage: storage });
 const bodyParser = require("body-parser");
-const myPrivateSecret = "secret";
 
-app.use(bodyParser.json()); //for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencoded
-app.use("/private", (req, res, next) => {
-  if (req.body.token) {
-    const token = req.body.token;
-    try {
-      const decoded = jwt.verify(token, myPrivateSecret);
-      req.body.decoded = decoded;
-      next();
-    } catch (e) {
-      console.log(e);
-      res.sendStatus(403);
-    }
-  } else {
-    res.sendStatus(403);
-  }
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/upload-files", upload.array("files"), (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ message: "Successfully uploaded files" });
 });
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-app.post("/public/login", (req, res) => {
-  const { user, password } = req.body;
-  //const user = await db.get(...);
-  if (user === "nodejs" && password === "mojeheslo") {
-    const token = jwt.sign({ userId: 5 }, myPrivateSecret, { expiresIn: 3600 });
-    res.json({ token });
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-app.post("/private/reporting", async (req, res) => {
-  try {
-    const { decoded } = req.body;
-    const userProfile = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${decoded.userId}`
-    );
-    console.log("HERE");
-    res.json({ userProfile: userProfile.data });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-app.post("/save", (req, res) => {
-  const { course } = req.body;
-  if (course === "NodeJS") {
-    res.json({
-      answer: "Hurááá",
-    });
-  } else {
-    res.json({
-      answer: "Máš být asi o třídu vedle",
-    });
-  }
-});
-
-app.put("/myput", (req, res) => {
-  res.sendStatus(404);
-});
-
-const port = 8080;
+const port = 3000;
 app.listen(port, "localhost");
 console.log(`server listening on port: ${port}`);
---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+const form = document.getElementById("form");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    const name = document.getElementById("name");
+    const files = document.getElementById("files");
+    const formData = new FormData();
+    formData.append("name", name.value);
+    for(let i =0; i < files.files.length; i++) {
+            formData.append("files", files.files[i]);
+    }
+    fetch("http://localhost:3000/api/upload-files", {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Accept': '*/*',
+          'Access-Control-Allow-Origin': '*'
+        }
+    })
+        .then((res) => {
+          window.alert("Odesláno");
+          console.log(res);
+        })
+        .catch((err) => ("Error occured", err));
+}
